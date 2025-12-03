94b485624ea6f8b2b118972241d84006d5a77d0c
Balekiala <balekialaj@gmail.com>
2025-12-03 01:46:53 +0100

    F1 formulaire avec texte + boutton

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,19 @@ function App() {
     <div>
       <h1>Bienvenue chez nous !</h1>
       <p>Connectez-vous</p>
+      <form className="flex flex-col gap-3 w-full max-w-sm">
+      <input
+        type="text"
+        placeholder="Entrez votre prénom..."
+        className="border p-2 rounded-xl"
+      />
+      <button
+        type="submit"
+        className="p-3 rounded-xl shadow font-semibold hover:opacity-90"
+      >
+        Accéder à votre espace
+      </button>
+      </form>
     </div>
   )
 }
