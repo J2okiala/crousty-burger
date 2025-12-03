96e0fb5ab28e0703e8bb8e188e3b666a622e0dba
Balekiala <balekialaj@gmail.com>
2025-12-03 19:53:16 +0100

    F1 Refacto LoginForm

---
+++ src/components/pages/login/LoginForm.jsx
@@ -0,0 +1,38 @@
+import { useState } from "react";
+
+
+export default function LoginForm() {
+
+    // state
+    const [prenom, setPrenom] = useState("")
+
+    // comportement
+    const handleSubmit = (e) => {
+        e.preventDefault();
+        alert(`Bonjour ${prenom}`);
+        setPrenom("")
+    };
+
+    const handleChange = (event) => {
+        setPrenom(event.target.value)
+    }
+
+    // affichage (render)
+    return (
+        <form action="submit" onSubmit={handleSubmit} className="flex flex-col gap-3 w-full max-w-sm">
+                    <h1>Bienvenue chez nous !</h1>
+                    <h2>Connectez-vous</h2>
+                    <input
+                        type="text"
+                        value={prenom}
+                        onChange={handleChange}
+                        placeholder="Entrez votre prénom..." 
+                        required
+                        className="border p-2 rounded-xl"
+                    />
+                    <button className="p-3 rounded-xl shadow font-semibold hover:opacity-90">
+                        Accéder à votre espace
+                    </button>
+                </form>
+    )
+}
