d8ae9b9f13d910adcbe245538e652f4e645b5909
Balekiala <balekialaj@gmail.com>
2025-12-06 15:36:33 +0100

    installation styled components et react-icons

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css'
+
+import LoginPage from './components/pages/login/LoginPage';
+import OrderPage from './components/pages/order/OrderPage';
+import ErrorPage from './components/pages/error/ErrorPage';
+
 
 function App() {
 
+
   return (
-    <div>
-      <h1>Bienvenue chez nous !</h1>
-      <p>Connectez-vous</p>
-      <form className="flex flex-col gap-3 w-full max-w-sm">
-      <input
-        type="text"
-        placeholder="Entrez votre prénom..."
-        className="border p-2 rounded-xl"
-      />
-      <button
-        type="submit"
-        className="p-3 rounded-xl shadow font-semibold hover:opacity-90"
-      >
-        Accéder à votre espace
-      </button>
-      </form>
-    </div>
+    <Routes>
+      <Route path='/' element={<LoginPage />} />
+      <Route path='/order/:prenom' element={<OrderPage />} />
+      <Route path='*' element={<ErrorPage />} />
+    </Routes>
+    
   )
 }
 
---
+++ src/components/pages/error/ErrorPage.jsx
@@ -0,0 +1,14 @@
+import { Link } from "react-router-dom";
+
+
+export default function ErrorPage() {
+    return (
+        <div>
+            <h2>ErrorPage</h2>
+            <Link to={'/'}>
+                <button>Retournez a la page d'accueil</button>
+            </Link>
+            
+        </div>
+    )
+}
